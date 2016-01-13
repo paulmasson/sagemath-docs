407ab2eb96aa72f539469b1ee1290aa4a9297357
paulmasson <paulmasson@comcast.net>
2016-01-12 18:04:28 -0800

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sagemath-docs
+
+Simple and clear documentation for SageMath functions and operations
+
+Main index page: http://paulmasson.github.io/sagemath-docs/index.html
+
+Parsing utilities courtesy of Theo Armour @jaanga  
+http://jaanga.github.io/demo/pm/get-sage-function-names/get-sage-function-names-r2.html
---
+++ setup.js
@@ -0,0 +1,21 @@
+sagecell.makeSagecell( {
+
+	inputLocation: '.sage',
+	autoeval: true,
+	hide: ['editor','evalButton','permalink']
+
+} );
+
+var check = requestAnimationFrame( accept );
+setTimeout( function() { cancelAnimationFrame( check ); }, 5000 );
+
+function accept() {
+
+	check = requestAnimationFrame( accept );
+
+	var buttons = document.getElementsByClassName( 'ui-dialog-buttonset' );
+	if ( buttons.length > 0 ) {
+		buttons[0].firstChild.click();
+		cancelAnimationFrame( check );
+	}
+}
