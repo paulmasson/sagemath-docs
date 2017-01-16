bb49818544411e13d58f53248c9c831fb3503628
Paul Masson <paulmasson@comcast.net>
2017-01-16 12:40:47 -0800

    Debug

--- setup.js
+++ setup.js
@@ -12,13 +12,13 @@ function resizeIframes() {
     if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
 
         var scenes = document.getElementsByTagName( 'iframe' );
-console.log('found '+scenes.length+' scenes');
+alert('found '+scenes.length+' scenes');
         for ( var i=0 ; i < scenes.length ; i++ ) {
 
             scenes[i].style.width = getComputedStyle( scenes[i] ).width;
             scenes[i].style.height = getComputedStyle( scenes[i] ).height;
             scenes[i].setAttribute( 'scrolling', 'no' );
-console.log('scene '+i+' width/height is '+scenes[i].style.width+'/'+scenes[i].style.height);
+alert('scene '+i+' width/height is '+scenes[i].style.width+'/'+scenes[i].style.height);
         }
     }
 
