29f96d665c8264711344ea0e86a643227da59a60
Paul Masson <paulmasson@comcast.net>
2017-01-16 13:36:57 -0800

    Update setup.js

--- setup.js
+++ setup.js
@@ -2,8 +2,7 @@ sagecell.makeSagecell( {
 
 	inputLocation: '.sagemathcell',
 	autoeval: true,
-	hide: ['editor','evalButton','permalink'],
-	callback: resizeIframes
+	hide: ['editor','evalButton','permalink']
 
 } );
 
@@ -12,13 +11,14 @@ function resizeIframes() {
     if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
 
         var scenes = document.getElementsByTagName( 'iframe' );
-alert('found '+scenes.length+' scenes');
+
         for ( var i=0 ; i < scenes.length ; i++ ) {
 
-            scenes[i].style.width = getComputedStyle( scenes[i] ).width;
-            scenes[i].style.height = getComputedStyle( scenes[i] ).height;
+//            scenes[i].style.width = getComputedStyle( scenes[i] ).width;
+//            scenes[i].style.height = getComputedStyle( scenes[i] ).height;
+    	    scenes[i].srcdoc = scenes[i].srcdoc;
             scenes[i].setAttribute( 'scrolling', 'no' );
-alert('scene '+i+' width/height is '+scenes[i].style.width+'/'+scenes[i].style.height);
+
         }
     }
 
